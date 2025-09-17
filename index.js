afb16442be9911ccc90de80bef4e72911da8a5af
Kajal Saini <sizukabts91@gmail.com>
2025-09-17 21:15:40 +0530

    create public folder

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const port = 3000
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 app.get('/about', (req, res) => {
-  //res.send('Hello World! I am the about page.')
-  res.sendFile(path.join(__dirname, 'index.html'))
+    res.sendFile(path.join(__dirname, 'index.html'))
 })
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
