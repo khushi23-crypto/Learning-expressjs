fa23c5a5e6d2e7efaa1d7101e75fe157d6d8acd6
Kajal Saini <sizukabts91@gmail.com>
2025-09-20 17:01:26 +0530

    Create one router

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World! hii bye hlwwwww')
 })
+app.get('/profile', (req, res) => {
+  res.send('This is my profile')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
