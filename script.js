ac25aae237f4a15d05e5b6dadb48cd7fe073d50a
Kajal Saini <sizukabts91@gmail.com>
2025-09-21 11:41:11 +0530

    learn template engine concept

--- script.js
+++ script.js
@@ -7,12 +7,16 @@ app.use(function(res,req,next){
     console.log("this is middleware")
     next()
 })
+app.set("view engine","ejs");
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World! hii bye hlwwwww')
+  res.render("index")
 })
 app.get('/profile', (req, res) => {
   res.send('This is my profile')
 })
+//author/books/issues/:username
 app.get('/profile/:username', (req, res) => {
   res.send(`Hello from all ${req.params.username}`)
 })
