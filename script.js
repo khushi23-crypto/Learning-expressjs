550f6421ffab9dacb1793d559efa3a42f70f313e
Kajal Saini <sizukabts91@gmail.com>
2025-09-21 12:47:54 +0530

    create error.ejs

--- script.js
+++ script.js
@@ -14,9 +14,23 @@ app.get('/', function (req, res) {
   let students="Kajal And Shubham";
   res.render("index",{name:students});
 });
+
 app.get('/contact', function(req, res) {
   res.render("contact")
-})
+});
+
+app.get('/error', function(req, res,next) {
+  throw Error("something went wrong")
+});
+
+app.use(function errorHandler (err, req, res, next) {
+  if (res.headersSent) {
+    return next(err)
+  }
+  res.status(500)
+  res.render('error', { error: err })
+}
+)
 //author/books/issues/:username
 /* app.get('/profile/:username', (req, res) => {
   res.send(`Hello from all ${req.params.username}`)
